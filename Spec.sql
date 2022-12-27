3a3e7d89354bc23380091498ca75b0fd91f0b953
sosdoba <saraosdoba1@gmail.com>
2022-12-26 20:39:45 -0500

    Spec.sql

---
+++ Spec.sql
@@ -0,0 +1,51 @@
+/*
+Since I was a child I've always loved baking and people have always told me that I should open a pastry buisness. 
+I've just started making plans, and will be opening it in January of 2023, G-d willing. 
+With all the orders that I am anticipating, I would like to have all the information stored in one place.
+And of course, being that I would like to start my buisness off on the right foot, everything must be as clear and concise and possible.
+
+I would like a table of all the orders that come in, including the customer's name, item and cost of the order. 
+To give a clearer idea, I will need the item, quantity, price per unit, total sum, date ordered, and date needed by.
+I would like to have the last 4 digits of their card number displayed in the chart.  
+It's also important for us to track how the order came through, whether it was via text, email, mail, etc.
+
+
+Questions:
+
+Q:How much in advance can an order come in to when it needs to be ready?
+A: Orders can be placed up to 60 days in advance and up to one week in advance. Orders with less than a week of notice are invalid.
+
+Q:Can orders be placed before your grand opening?
+A:Yes! We would love to have customers order from us in advance so that they can enjoy are baked goods on the first day we open!
+
+Q:What is your price range for your goods?
+A:Our items range from $2.50 - $450. Once things settle down I plan to add few grander options, with prices going as high as $650.
+
+Q:What are the ways that customers can place an order?
+A:Customers have 4 options. They can either call, text, mail, or email us.
+
+Q:Is there any other way customers can pay?
+A:They can pay with cash or card.
+
+Q:Do you deliver?
+A:Unfortunately we do not deliver, customers need to come pick up their orders.
+
+Reports:
+1. I will need a list of my top 3 goods that are the most popular.
+2. I would like to know how many customers are paying with cash and how many with card, just so I can know how to better service them. (Eventually I would like to switch over to card only.)
+3. I would like to have a concise list of the date, item, quantity, and order total so that I can better understand what's poplular at different times of year.
+
+Sample Data:
+Brian Johns, Wedding cookies, 75, $4.5, $337.50, December 9 2022, January 3 2023, 0383, Call
+John Seba, Birthday Cake, 1, $115, $115, December 12 2022, February 1 2023, 6393, Email
+Sarah Will, Number Cake, 2, $100, $200, December 25 2022, January 22 2023, cash, Email
+Josh Brad, Birthday cupcakes, 35, $3.5, $122.50, November 29 2022, January 5 2023 8639, Text
+Mike Seris, Wedding Cake, 1, $450, $450, December 1 2022, January 29 2023, 8363, Call
+Steve Woods, Baby Girl cookies, 50, $3, $150, November 9 2022, January 2 2023, 7238, Text
+Mike Brand, Baby Boy cupcakes, 45, $3.50, $157.50, December 15 2022, February 1 2023, Call
+Emma Bird, Wedding cake (small), 1, $350, $350, December 1 2022, January 15 2023, Email
+Emma Bird, Wedding cupcakes, 75, $5.50, $412.5, December 1 2022, January 15 2023, Email
+Emma Bird, Wedding cookies 100, $6.50 $650, December 1 2022, January 15 2023, Email
+Brian Sars, Birthday cake, 1, $115, $115, December 26 2022, January 31 2023, Text
+
+*/
